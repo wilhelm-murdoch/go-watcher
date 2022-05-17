dd6539f6ccf3ce170e234a638345f2bf5d88bfe7
Wilhelm Murdoch <wilhelm@devilmayco.de>
2022-05-18 02:04:03 +1000

    wrapping up testing

--- watcher.go
+++ watcher.go
@@ -14,19 +14,15 @@ import (
 // Watcher represents a wrapper around `fsnotify` complete with support for its
 // own callbacks for all supported event types.
 type Watcher struct {
-	fsnotify                                       *fsnotify.Watcher                              // Instance of `fsnotify` wrapped by this package.
-	done                                           chan bool                                      // A signal channel used to exit the wait loop.
-	hasCallbacks                                   bool                                           // True if any callbacks have been assigned to any supported `fsnotify.Event` event.
-	onRemove, onCreate, onWrite, onRename, onChmod func(os.FileInfo, error) error                 // Dedicated optional callback functions for each specific `fsnotify.Event` type.
-	onAll                                          func(fsnotify.Event, os.FileInfo, error) error // Dedicated optional callback function used to catch all `fsnotify.Event` types.
+	fsnotify                                              *fsnotify.Watcher                              // Instance of `fsnotify` wrapped by this package.
+	done                                                  chan bool                                      // A signal channel used to exit the wait loop.
+	hasCallbacks                                          bool                                           // True if any callbacks have been assigned to any supported `fsnotify.Event` event.
+	onAll, onRemove, onCreate, onWrite, onRename, onChmod func(fsnotify.Event, os.FileInfo, error) error // Dedicated optional callback functions for each specific `fsnotify.Event` type.
 }
 
 // New creates a new instance of a Watcher struct.
 func New() (*Watcher, error) {
-	fsn, err := fsnotify.NewWatcher()
-	if err != nil {
-		return nil, err
-	}
+	fsn, _ := fsnotify.NewWatcher()
 
 	return &Watcher{
 		fsnotify: fsn,
@@ -36,22 +32,20 @@ func New() (*Watcher, error) {
 
 // AddFile adds a single valid file to the current Watcher instance and returns
 // an error if the file is not valid.
-func (w *Watcher) AddFile(path string) error {
+func (w *Watcher) AddPath(path string) error {
 	return w.fsnotify.Add(path)
 }
 
 // AddDir will recursively walk the specified directory tree and add all valid
 // files to the current watcher instance for monitoring.
-func (w *Watcher) AddDir(path string) error {
+func (w *Watcher) WalkPath(path string) error {
 	err := filepath.WalkDir(path, func(path string, entry fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
 		if entry.IsDir() {
-			if err = w.AddFile(path); err != nil {
-				return err
-			}
+			w.AddPath(path)
 		}
 
 		return nil
@@ -69,9 +63,7 @@ func (w *Watcher) AddGlob(pattern string) error {
 	}
 
 	for _, file := range files {
-		if err := w.AddFile(file); err != nil {
-			return err
-		}
+		w.AddPath(file)
 	}
 
 	return nil
@@ -80,7 +72,7 @@ func (w *Watcher) AddGlob(pattern string) error {
 // On fires off an assigned callback for each event type. Only specified events
 // are supported and all will return either nil or an error. Every watcher
 // instance exits when it first encounters an error.
-func (w *Watcher) On(event fsnotify.Op, f func(os.FileInfo, error) error) error {
+func (w *Watcher) On(event fsnotify.Op, f func(fsnotify.Event, os.FileInfo, error) error) error {
 	switch event {
 	case fsnotify.Write:
 		w.onWrite = f
@@ -113,7 +105,7 @@ func (w *Watcher) All(f func(fsnotify.Event, os.FileInfo, error) error) {
 func (w *Watcher) Watch() error {
 	var group errgroup.Group
 
-	if len(w.fsnotify.WatchList()) == 0 {
+	if len(w.List()) == 0 {
 		return errors.New("no files specified to watch")
 	}
 
@@ -129,23 +121,23 @@ func (w *Watcher) Watch() error {
 				switch {
 				case event.Op&fsnotify.Write == fsnotify.Write:
 					if w.onWrite != nil {
-						err = w.onWrite(info, err)
+						err = w.onWrite(event, info, err)
 					}
 				case event.Op&fsnotify.Create == fsnotify.Create:
 					if w.onCreate != nil {
-						err = w.onCreate(info, err)
+						err = w.onCreate(event, info, err)
 					}
 				case event.Op&fsnotify.Remove == fsnotify.Remove:
 					if w.onRemove != nil {
-						err = w.onRemove(info, err)
+						err = w.onRemove(event, info, err)
 					}
 				case event.Op&fsnotify.Rename == fsnotify.Rename:
 					if w.onRename != nil {
-						err = w.onRename(info, err)
+						err = w.onRename(event, info, err)
 					}
 				case event.Op&fsnotify.Chmod == fsnotify.Chmod:
 					if w.onChmod != nil {
-						err = w.onChmod(info, err)
+						err = w.onChmod(event, info, err)
 					}
 				}
 
@@ -169,6 +161,13 @@ func (w *Watcher) Watch() error {
 	})
 
 	return group.Wait()
+}
+
+// List is a wrapper around `fsnotify.Watchlist()`. It returns a list of strings
+// representing all files and directories currently monitored instance of
+// `fsnotify`.
+func (w *Watcher) List() []string {
+	return w.fsnotify.WatchList()
 }
 
 // Done signals a blocking channel that processing is complete and that we can
--- watcher_test.go
+++ watcher_test.go
@@ -1,18 +1,82 @@
 package watcher_test
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
+	"time"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/stretchr/testify/assert"
 	"github.com/wilhelm-murdoch/go-watcher"
 )
 
-func TestWatcherNoCallbacks(t *testing.T) {
-	var err error
+var events = []fsnotify.Op{
+	fsnotify.Write,
+	fsnotify.Create,
+	fsnotify.Remove,
+	fsnotify.Rename,
+	fsnotify.Chmod,
+}
+
+func createFile(path string) (string, error) {
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		return "", err
+	}
+
+	file, err := ioutil.TempFile(path, "prefix")
+	if err != nil {
+		return "", err
+	}
+
+	return file.Name(), nil
+}
+
+func createFiles(path string, count int) ([]string, error) {
+	var files []string
+
+	for i := 1; i <= count; i++ {
+		file, err := createFile(path)
+		if err != nil {
+			return nil, err
+		}
+		files = append(files, file)
+	}
+
+	return files, nil
+}
+
+func touchFile(path string) error {
+	t := time.Now().Local()
+	err := os.Chtimes(path, t, t)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func cleanFiles(path string) error {
+	return os.RemoveAll(path)
+}
+
+func TestWatcherList(t *testing.T) {
+	defer cleanFiles("./test_files")
+
 	w, err := watcher.New()
 	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
 
-	err = w.AddFile("test.txt")
+	createFiles("./test_files", 5)
+
+	assert.Nil(t, w.AddPath("./test_files"), "was expecting no errors, but got %s instead", err)
+	assert.Equal(t, 6, len(w.List()), "was expecting %d items, but got %d instead", 6, len(w.List()))
+}
+
+func TestWatcherNoCallbacks(t *testing.T) {
+	w, err := watcher.New()
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
+
+	err = w.AddPath("watcher_test.go")
 	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
 
 	err = w.Watch()
@@ -20,34 +84,126 @@ func TestWatcherNoCallbacks(t *testing.T) {
 }
 
 func TestWatcherNoFiles(t *testing.T) {
-	var err error
 	w, err := watcher.New()
 	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
+
+	err = w.Watch()
+	assert.NotNil(t, err, "was expecting no errors, but got %s instead", err.Error())
+	assert.Zero(t, len(w.List()), "was expecting zero results, but got %d instead", len(w.List()))
+}
+
+func TestWatcherWalkPath(t *testing.T) {
+	defer cleanFiles("./test_files")
+
+	w, err := watcher.New()
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
+
+	createFiles("./test_files", 5)
+	createFiles("./test_files/sub", 5)
+
+	err = w.WalkPath("./test_files")
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
+	assert.Equal(t, 12, len(w.List()), "was expecting %d items, but got %d instead", 12, len(w.List()))
+}
+
+func TestWatcherWalkPathInvalidFile(t *testing.T) {
+	w, err := watcher.New()
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
+
+	err = w.WalkPath("./i-do-not-exist")
+	assert.NotNil(t, err, "was expecting a pattern error, but got nothing instead")
+}
+
+func TestWatcherGlob(t *testing.T) {
+	defer cleanFiles("./test_files")
+
+	w, err := watcher.New()
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
+
+	createFiles("./test_files/sub", 5)
+
+	err = w.AddGlob("./test_files/**/prefix*")
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
+	assert.Equal(t, 5, len(w.List()), "was expecting %d items, but got %d instead", 5, len(w.List()))
 
 	err = w.Watch()
 	assert.NotNil(t, err, "was expecting no errors, but got %s instead", err)
 }
 
-func TestWatcherWalkDir(t *testing.T) {
+func TestWatcherGlobInvalidPattern(t *testing.T) {
+	w, err := watcher.New()
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
 
+	err = w.AddGlob("a[")
+	assert.NotNil(t, err, "was expecting a pattern error, but got nothing instead")
 }
 
-func TestWatcherGlob(t *testing.T) {
+func TestWatcherOnCallbackSet(t *testing.T) {
+	w, err := watcher.New()
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
 
+	callback := func(event fsnotify.Event, file os.FileInfo, err error) error {
+		return nil
+	}
+
+	for _, e := range events {
+		err := w.On(e, callback)
+		assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
+	}
+
+	const BadOp fsnotify.Op = 000
+	err = w.On(BadOp, callback)
+	assert.NotNil(t, err, "was expecting an error, but got nothing instead")
 }
 
-func TestWatcherOn(t *testing.T) {
+func TestWatcherAll(t *testing.T) {
+	w, err := watcher.New()
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
 
+	w.All(func(event fsnotify.Event, file os.FileInfo, err error) error {
+		return nil
+	})
 }
 
-func TestWatcherAl(t *testing.T) {
+func TestWatcherWatchOn(t *testing.T) {
+	defer cleanFiles("watch_path")
+	var message chan string
+	var finished chan bool
 
-}
+	w, err := watcher.New()
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
 
-func TestWatcherWatch(t *testing.T) {
+	callback := func(event fsnotify.Event, file os.FileInfo, err error) error {
+		message <- fmt.Sprintf("%s: %s", event.Name, file.Name())
+		return nil
+	}
 
-}
+	for _, e := range events {
+		err := w.On(e, callback)
+		assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
+	}
 
-func TestWatcherDone(t *testing.T) {
+	err = os.MkdirAll("watch_path", os.ModePerm)
+	assert.Nil(t, err, "was expecting no errors, but got %s instead", err)
 
+	w.AddPath("watch_path")
+
+	go func() {
+		for {
+			select {
+			case m := <-message:
+				fmt.Println(m)
+			case <-finished:
+				fmt.Println("sup")
+				w.Done()
+				close(message)
+				close(finished)
+			}
+		}
+	}()
+
+	// go w.Watch()
+
+	finished <- true
+	fmt.Println("sup")
 }
